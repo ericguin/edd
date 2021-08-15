965f153592102b2fa15dcefaabb1afad2b0a32c1
ericguin <eric.guinn@outlook.com>
2021-08-15 10:00:00 -0600

    Initial structure working

---
+++ edd/components/argparse/src/argparse.cc
@@ -0,0 +1,6 @@
+#include "argparse.hh"
+
+void func()
+{
+    std::cout << "This is argparse" << std::endl;
+}
---
+++ edd/main.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "argparse.hh"
+
+int main(int argc, char** argv)
+{
+    std::cout << "Yes" << std::endl;
+    func();
+}
